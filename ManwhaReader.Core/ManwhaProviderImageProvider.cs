bccea3ad8e6e436aa1e0ee6a06cfd7d9ee434b56
Tomas <tomaskeder2005@gmail.com>
2024-12-26 00:15:45 +0100

    Removed Thunderscans

--- ManwhaReader.Core/ManwhaProviderImageProvider.cs
+++ ManwhaReader.Core/ManwhaProviderImageProvider.cs
@@ -4,6 +4,5 @@ public class ManwhaProviderImageProvider
 {
     private const string BaseUrl = "/Images/Providers/";
     
-    public static string Thunderscans => $"{BaseUrl}thunderscans.png";
     public static string MangaDex => $"{BaseUrl}mangadex.png";
 }
