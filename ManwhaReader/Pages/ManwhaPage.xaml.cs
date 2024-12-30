ad84d0545d95c987c42983409a92d747e68716c9
Tomas <tomaskeder2005@gmail.com>
2024-12-31 00:46:13 +0100

    Adjusted

--- ManwhaReader/Pages/ManwhaPage.xaml.cs
+++ ManwhaReader/Pages/ManwhaPage.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using ManwhaReader.Core.DatabaseObjects;
 
 namespace ManwhaReader.Pages;
@@ -7,10 +8,22 @@ public partial class ManwhaPage : Page
 {
     public ManwhaPage(Manwha manwha)
     {
+        ArgumentNullException.ThrowIfNull(manwha);
+
         InitializeComponent();
         
         Manwha = manwha;
+        SetContent();
     }
-
+    
     private Manwha Manwha { get; }
+    
+    private void SetContent()
+    {
+        thumbnail.Source = new BitmapImage(new Uri(Manwha.ThumbnailUrl));
+        title.Text = Manwha.Name;
+        tags.ItemsSource = Manwha.Tags;
+        status.Text = Manwha.Status;
+        description.Text = Manwha.Description!;
+    }
 }
