3e8369900afb037f451f1fdff6da57067de3e0d1
Tomas <tomaskeder2005@gmail.com>
2024-12-30 01:14:17 +0100

    Implemented GetManwhaByTitle logic

--- ManwhaReader.Core/MangadexManwhaProvider.cs
+++ ManwhaReader.Core/MangadexManwhaProvider.cs
@@ -1,3 +1,5 @@
+using DevExpress.Xpo;
+using ManwhaReader.Core.DatabaseObjects;
 using ManwhaReader.Core.Interfaces;
 using Newtonsoft.Json.Linq;
 
@@ -6,7 +8,7 @@ namespace ManwhaReader.Core;
 public class MangadexManwhaProvider : IManwhaProvider
 {
     public string Name => "MangaDex";
-    public string ImageUrl => ManwhaProviderImagePaths.MangaDex;
+    public string ImageUrl => ImagePathProvider.ProviderMangaDex;
     
     public async Task<IEnumerable<IManwhaSearchResult>> Search(string searchQuery, bool loadImages = true)
     {
@@ -40,15 +42,67 @@ public class MangadexManwhaProvider : IManwhaProvider
         return list;
     }
 
+    public async Task<Manwha> GetManwhaByTitle(string title)
+    {
+        if (string.IsNullOrWhiteSpace(title))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(title));
+        
+        using var client = CreateHttpClient();
+        var url = $"https://api.mangadex.org/manga?title={title}";
+        var jsonString = await client.GetStringAsync(url);
+        
+        var jsonArray = JArray.Parse(jsonString)["data"];
+
+        if (jsonArray == null)
+            throw new InvalidOperationException("Response data cannot be null.");
+        
+        var jObject = jsonArray.FirstOrDefault(x => string.Equals(title, x["attributes"]?["title"]?.First?.First?.ToString(), StringComparison.InvariantCultureIgnoreCase));
+        
+        if(jObject == null)
+            throw new Exception($"Couldn't find manga for title '{title}'\nPlease report this.");
+        
+        var description = jObject["attributes"]?["description"]?.First?.First?.ToString();
+        var status = jObject["attributes"]?["status"]?.ToString();
+        var tags = new List<string>();
+
+        if (jObject["attributes"]?["tags"] is JArray tagsArray)
+        {
+            tags.AddRange(tagsArray.Select(tagObject => tagObject["attributes"]?["name"]?.First?.First?.ToString()!)
+                .Where(tag => !string.IsNullOrWhiteSpace(tag)));
+        }
+        
+        var manwhaId = jObject["id"]?.ToString();
+        var coverId = jObject["relationships"]?.FirstOrDefault(x => string.Equals("cover_art", x["type"]?.ToString(), StringComparison.InvariantCultureIgnoreCase))?["id"]?.ToString();
+        var thumbnailUrl = GetManwhaCoverImageUrl(manwhaId, coverId);
+
+        return new Manwha(Session.DefaultSession)
+        {
+            Name = title,
+            ThumbnailUrl = thumbnailUrl,
+            Description = description,
+            Status = status,
+            Tags = tags
+        };
+    }
+
     private static byte[] GetManwhaCoverImageData(string manwhaId, string coverId)
     {
         using var client = CreateHttpClient();
+        return client.GetByteArrayAsync(GetManwhaCoverImageUrl(manwhaId, coverId)).Result;
+    }
+
+    private static string GetManwhaCoverImageUrl(string? manwhaId, string? coverId)
+    {
+        if (string.IsNullOrWhiteSpace(manwhaId) || string.IsNullOrWhiteSpace(coverId))
+            return ImagePathProvider.ThumbnailNotFound;
+        
+        using var client = CreateHttpClient();
+        
         var coverUrlRequest = $"https://api.mangadex.org/cover/{coverId}";
         var coverUrlResponse = client.GetStringAsync(coverUrlRequest).Result;
+        var fileName = JObject.Parse(coverUrlResponse)["data"]?["attributes"]?["fileName"]?.ToString();
         
-        var coverFileName = JObject.Parse(coverUrlResponse)["data"]?["attributes"]?["fileName"]?.ToString();
-        
-        return client.GetByteArrayAsync($"https://uploads.mangadex.org/covers/{manwhaId}/{coverFileName}").Result;
+        return $"https://uploads.mangadex.org/covers/{manwhaId}/{fileName}";
     }
     
     private static HttpClient CreateHttpClient()
